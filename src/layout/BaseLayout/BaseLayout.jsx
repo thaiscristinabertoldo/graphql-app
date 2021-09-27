efd5d0da6a95cc3a947a559d89e03345547cb058
thais.bertoldo <thais.bertoldo@softplan.com.br>
2021-09-26 21:32:56 -0300

    feat: adiciona toast

--- src/layout/BaseLayout/BaseLayout.jsx
+++ src/layout/BaseLayout/BaseLayout.jsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from "react-toastify";
+
 import PropTypes from "prop-types";
 
 import { Header } from "../../components";
@@ -6,6 +8,19 @@ import { LayoutContainer } from "./BaseLayout.styles";
 
 export const BaseLayout = ({ children }) => (
   <Theme>
+    <ToastContainer
+      position="bottom-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      rtl={false}
+      theme="dark"
+      closeOnClick
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+
     <Header />
 
     <LayoutContainer>{children}</LayoutContainer>
